cd259f1cfa2a51eb087ea7e456df3bc6c11a67a9
mhidaka <rabbitlog@gmail.com>
2015-02-14 22:25:06 +0900

    hamu.jsを追加 　server.getLastData を追加

---
+++ js/hamu.js
@@ -0,0 +1,34 @@
+var server = (function () {
+
+  function CloudPoint() {
+    cloud　= this;
+    this.HOST = 'http://firefox-team9.azurewebsites.net/hamster';
+
+    this.xhr = function(method, url, data, success, error){
+        var ajax = new XMLHttpRequest();
+        ajax.open(method, url,true);
+        ajax.onreadystatechange = function Receive() {
+          if (ajax.readyState == 4 && ajax.status == 200){
+              success(ajax.responseText);
+          } else if (ajax.status != 200) {
+              error("Loading failure");
+          }
+        };
+        ajax.send(data);
+      };
+
+      this.getLastData = function(){
+        var script = document.createElement('script');
+        script.src = cloud.HOST + '/get_latest?callback=server.callback';
+        document.body.appendChild(script);
+      };
+
+      this.callback = function(data){
+          console.log(data);
+          json = JSON.stringify(data);
+          alert(json);
+      };
+  }
+  return new CloudPoint();
+})();
+
